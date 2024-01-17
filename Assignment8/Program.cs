b0e12919fb8d099a23ecbe010fc8793097e215c5
Mittaanjali <mitta.anjali@mphasis.com>
2024-01-17 16:45:11 +0530

    Assignment8

---
+++ Assignment8/Program.cs
@@ -0,0 +1,29 @@
+using Assignment8.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<LibraryDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("BookConStr")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
